ce8dabf89f48e27d1675072f588063de8ce0e0ec
Kevin Wiltschnig <kevinwiltschnig@Kevins-MacBook-Air.local>
2018-10-08 14:56:46 +0200

    added method multiplyByAdding

--- src/main/java/Einstiegsbeispiel.java
+++ src/main/java/Einstiegsbeispiel.java
@@ -1,2 +1,21 @@
 public class Einstiegsbeispiel {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(multiplyByAdding(10,102));
+
+    }
+
+
+    static int multiplyByAdding(int a, int b) {
+        int temp = 0;
+        int count = b;
+
+        while(count > 0) {
+            temp += a;
+            count--;
+        }
+        return temp;
+    }
 }
