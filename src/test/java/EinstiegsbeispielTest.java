34a96cdcb7bcc43c9a4f3a654c3fee03a3fa426d
Kevin Wiltschnig <kevinwiltschnig@Kevins-MacBook-Air.local>
2018-10-09 15:59:30 +0200

    programming of the test method

--- src/test/java/EinstiegsbeispielTest.java
+++ src/test/java/EinstiegsbeispielTest.java
@@ -1,2 +1,16 @@
-public class EinstiegsbeispielTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EinstiegsbeispielTest {
+
+    @Test
+    public void test(){
+        Einstiegsbeispiel x = new Einstiegsbeispiel();
+
+
+        int b = x.multiplyByAdding(5,4);
+        assertEquals(20,b);
+    }
+
 }
