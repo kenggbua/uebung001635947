20fb515e0b011ac20f4f29ab1cfdf157c56d2f09
kevinwiltschnig <kevin.wiltschnig@hotmail.com>
2018-10-15 16:01:02 +0200

    programming of 2nd test

--- src/test/java/EinstiegsbeispielTest.java
+++ src/test/java/EinstiegsbeispielTest.java
@@ -5,12 +5,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class EinstiegsbeispielTest {
 
     @Test
-    public void test(){
-        Einstiegsbeispiel x = new Einstiegsbeispiel();
+    public void test1multiplyByAddition(){
+        Einstiegsbeispiel positivetest = new Einstiegsbeispiel();
 
 
-        int b = x.multiplyByAdding(5,4);
+        int b = positivetest.multiplyByAdding(5,4);
         assertEquals(20,b);
     }
+
+    @Test
+    public void test2multiplyByAddition(){
+        Einstiegsbeispiel positivetest = new Einstiegsbeispiel();
+
+
+        int b = positivetest.multiplyByAdding(100,2523);
+        assertEquals(252300,b);
+    }
+
+
 
 }
