e9e424f28c0823abedcdf502c16afa96604c7892
kevinwiltschnig <kevin.wiltschnig@hotmail.com>
2018-10-24 09:31:59 +0200

    adding and programming of wrong input cases

--- src/main/java/Einstiegsbeispiel.java
+++ src/main/java/Einstiegsbeispiel.java
@@ -1,18 +1,22 @@
 public class Einstiegsbeispiel {
 
 
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
 
         System.out.println(multiplyByAdding(10,102));
 
-    }
+    } */
 
 
     static int multiplyByAdding(int a, int b) {
         int temp = 0;
         int count = b;
+        if(a <= 0 || b <= 0) {                  //checks if a and b are positive numbers
+            return -1;
+        }
 
-        while(count > 0) {
+
+        while(count > 0) {                      //adds the number a, b times
             temp += a;
             count--;
         }
--- src/test/java/EinstiegsbeispielTest.java
+++ src/test/java/EinstiegsbeispielTest.java
@@ -1,11 +1,26 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EinstiegsbeispielTest {
 
+    Einstiegsbeispiel init;
+
+    @BeforeEach
+    public void setup() {
+        init = new Einstiegsbeispiel();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        init = null;
+    }
+
+
     @Test
-    public void test1multiplyByAddition(){
+    public void positiveTest1multiplyByAddition(){
         Einstiegsbeispiel positivetest = new Einstiegsbeispiel();
 
 
@@ -14,7 +29,7 @@ class EinstiegsbeispielTest {
     }
 
     @Test
-    public void test2multiplyByAddition(){
+    public void positiveTest2multiplyByAddition(){
         Einstiegsbeispiel positivetest = new Einstiegsbeispiel();
 
 
@@ -23,5 +38,27 @@ class EinstiegsbeispielTest {
     }
 
 
+    @Test
+    public void wrongInput1multiplyByAddition(){
+        Einstiegsbeispiel positivetest = new Einstiegsbeispiel();
+
+
+        int b = positivetest.multiplyByAdding(-5,2523);
+        assertEquals(-1,b);
+
+        System.out.println("Die Eingabe ist nicht korrekt! Beide Eingabewerte m̈ussen gr̈oßer als 0 sein!");
+    }
+
+
+    @Test
+    public void wrongInput2multiplyByAddition(){
+        Einstiegsbeispiel positivetest = new Einstiegsbeispiel();
+
+
+        int b = positivetest.multiplyByAdding(100,-5135);
+        assertEquals(-1,b);
+
+        System.out.println("Die Eingabe ist nicht korrekt! Beide Eingabewerte m̈ussen gr̈oßer als 0 sein!");
+    }
 
 }
